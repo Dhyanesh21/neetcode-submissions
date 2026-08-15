f1e3706ffe95934f702b7902b438b5bd30722b19
Dhyanesh21 <113417933+Dhyanesh21@users.noreply.github.com>
2026-08-15 13:21:42 +0530

    Add: car-fleet - submission-2

---
+++ Data Structures & Algorithms/car-fleet/submission-2.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int carFleet(int target, vector<int>& position, vector<int>& speed) {
+        vector<pair<int,int>> cars;
+        int n = position.size();
+        for(int i=0;i<n;i++){
+            cars.push_back({position[i],speed[i]});
+        }
+        sort(cars.rbegin(),cars.rend());
+
+        int fleets = 0;
+        double lastTime = 0;
+        for(int i=0;i<n;i++){
+            double time = double(target - cars[i].first)/cars[i].second;
+            if(time > lastTime){
+                fleets++;
+                lastTime = time;
+            }
+        }
+        return fleets;
+    }
+};
